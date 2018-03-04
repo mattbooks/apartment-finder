df6a5ad9547a009954af451644886ccb9979331c
Matt Books <mattebooks@gmail.com>
2018-03-04 19:43:55 +0000

    Filter listings if >50% images are previously seen

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source "https://rubygems.org"
 
+gem 'http'
 gem 'nokogiri'
 gem 'tf-idf-similarity'
+gem 'dhash-vips'
--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,15 @@
 #encoding: utf-8
+
+FILTER_SIMILAR_IMAGES=true
+DEBUG=false
+
 require 'nokogiri'
 require 'http'
 require 'cgi'
 require 'fileutils'
 require 'matrix'
 require 'tf-idf-similarity'
+require 'dhash-vips' if FILTER_SIMILAR_IMAGES
 
 site = ARGV.shift
 subsite = ARGV.shift
@@ -17,6 +22,14 @@ raise "Can't find dir '#{target}'" unless Dir.exist?(target)
 
 corpus = Dir.glob("#{target}/*/description").map do |f|
   TfIdfSimilarity::Document.new(File.open(f, &:read).force_encoding('utf-8'))
+end
+
+fingerprints = []
+if FILTER_SIMILAR_IMAGES
+  fingerprints += Dir.glob('#{target}/*/fingerprints/*').
+    map { |x| x.split('/')}.
+    map(&:last).
+    map(&:to_i)
 end
 
 url = "http://#{site}.craigslist.org/search/#{subsite}/apa?hasPic=1&bundleDuplicates=1&min_price=#{min_price}&max_price=#{max_price}&#{options}format=rss"
@@ -47,9 +60,11 @@ listings.each do |listing|
   id = link.split('/').last.sub('.html', '')
   target_dir = "#{target}/#{id}"
   image_dir = "#{target_dir}/images"
+  fingerprint_dir = "#{target_dir}/fingerprints"
   next if Dir.exist?(target_dir)
 
   FileUtils.mkdir_p(image_dir)
+  FileUtils.mkdir_p(fingerprint_dir)
 
   File.open("#{target_dir}/title", 'w') do |f|
     f.puts "#{hood} - #{price} - #{attrs.join(" / ")}"
@@ -57,8 +72,30 @@ listings.each do |listing|
 
   description += "\n<br><br>"
   description += "\n<a href=\"#{link}\">VIEW</a>"
+  new_fingerprints = []
+  fingerprints_seen = 0
+
   images.each do |image|
     description += "\n<img src='#{image}'></img>"
+
+    image_path = "#{image_dir}/#{image.split('_')[1]}"
+    `curl #{image} 2>/dev/null > #{image_path}`
+
+    if FILTER_SIMILAR_IMAGES
+      hash = DHashVips::IDHash.fingerprint image_path
+      new_fingerprints << hash
+      FileUtils.touch "#{fingerprint_dir}/#{hash}"
+
+      fingerprints.each do |existing|
+        if DHashVips::IDHash.distance3(existing, hash) < 15
+          fingerprints_seen += 1
+          STDERR.puts "SEEN #{image_path} ALREADY" if DEBUG
+          break
+        end
+      end
+
+      fingerprints += new_fingerprints
+    end
   end
 
   File.open("#{target_dir}/description", 'w') do |f|
@@ -81,5 +118,15 @@ listings.each do |listing|
     end
   end
 
-  puts target_dir unless seen
+  STDERR.puts "fp seen: #{fingerprints_seen} images: #{images.size}" if DEBUG
+
+  if fingerprints_seen * 1.0 / images.size > 0.5
+    seen = true
+  end
+
+  if seen
+    STDERR.puts "SKIPPING #{target_dir}" if DEBUG
+  else
+    puts target_dir
+  end
 end
